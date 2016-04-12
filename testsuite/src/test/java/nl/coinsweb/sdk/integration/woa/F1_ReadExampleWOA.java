45595a4e01f30302bb1aeb3428fb76c9415a5365
Bastiaan Bijl <bastiaan@sysunite.com>
2016-04-12 16:21:56 +0200

    Load woa file as special woa model.

--- testsuite/src/test/java/nl/coinsweb/sdk/integration/woa/F1_ReadExampleWOA.java
+++ testsuite/src/test/java/nl/coinsweb/sdk/integration/woa/F1_ReadExampleWOA.java
@@ -2,7 +2,6 @@ package nl.coinsweb.sdk.integration.woa;
 
 import nl.coinsweb.sdk.integration.DatasetAsserts;
 import nl.coinsweb.sdk.integration.IntegrationHelper;
-import nl.coinsweb.sdk.jena.InMemCoinsContainer;
 import nl.coinsweb.sdk.jena.JenaCoinsContainer;
 import nl.coinsweb.sdk.jena.TDBCoinsContainer;
 import org.junit.FixMethodOrder;
@@ -22,25 +21,21 @@ public class F1_ReadExampleWOA {
 
 
   @Test
-  public void aCreateContainer() {
+  public void aOpenContainer() {
 
-    JenaCoinsContainer model = new TDBCoinsContainer("http://zhejiangdayuan.com/");
+    JenaCoinsContainer model = new TDBCoinsContainer("http://playground.com/");
 
     model.load(IntegrationHelper.getResourceFile("F1", "WOAVoorbeeld.ccr").getAbsolutePath());
 
+    log.info("instance model");
+    DatasetAsserts.logTriples(model.getJenaModel("http://www.buildingbits.nl/MBIContainer.rdf#"));
 
+    log.info("woa model");
+    DatasetAsserts.logTriples(model.getJenaModel("http://www.coinsweb.nl/voorbeeld#"));
 
-
-    DatasetAsserts.logTriples(model.getJenaModel());
+    model.close();
   }
 
-
-//  @Test
-  public void bReopenCcr() {
-
-    JenaCoinsContainer model = new InMemCoinsContainer("/tmp/coinstest/zhejiangdayuan.ccr","http://zhejiangdayuan.com/");
-    DatasetAsserts.logTriples(model.getJenaModel());
-  }
 
 
 
