6eef40157b75450bd6f64647e47745461f7b0446
Bastiaan Bijl <bastiaan@sysunite.com>
2016-04-14 23:03:12 +0200

    Proper WOA interpretation

--- testsuite/src/test/java/nl/coinsweb/sdk/integration/woa/F1_ReadExampleWOA.java
+++ testsuite/src/test/java/nl/coinsweb/sdk/integration/woa/F1_ReadExampleWOA.java
@@ -1,5 +1,6 @@
 package nl.coinsweb.sdk.integration.woa;
 
+import nl.coinsweb.cbim.Assembly;
 import nl.coinsweb.cbim.Part;
 import nl.coinsweb.sdk.exceptions.WOAAccessDeniedException;
 import nl.coinsweb.sdk.integration.DatasetAsserts;
@@ -34,13 +35,8 @@ public class F1_ReadExampleWOA {
 
     model.load(IntegrationHelper.getResourceFile("F1", "WOAVoorbeeld.ccr").getAbsolutePath());
 
-
-
-
     Part landhoofd = new Part(model, "http://www.buildingbits.nl/validatieContainer.rdf#_BB526node1a1hg7ekvx25");
     log.debug(landhoofd.getDescription());
-
-
 
     expectedEx.expect(WOAAccessDeniedException.class);
     expectedEx.expectMessage("WOA restriction blocked operation.");
@@ -48,11 +44,8 @@ public class F1_ReadExampleWOA {
     Part fundering = new Part(model, "http://www.buildingbits.nl/validatieContainer.rdf#_BB846node1a1hg7ekvx13");
     log.debug(fundering.getDescription());
 
-    Part viaduct = new Part(model, "http://www.buildingbits.nl/validatieContainer.rdf#_BB649node1a1hg7ekvx5");
+    Assembly viaduct = new Assembly(model, "http://www.buildingbits.nl/validatieContainer.rdf#_BB649node1a1hg7ekvx5");
     log.debug(viaduct.getDescription());
-
-
-
 
     log.info("instance model");
     DatasetAsserts.logTriples(model.getJenaModel("http://www.buildingbits.nl/MBIContainer.rdf#"));
@@ -64,6 +57,31 @@ public class F1_ReadExampleWOA {
   }
 
 
+  @Test
+  public void bDisabledWOA() {
 
+    JenaCoinsContainer model = new InMemCoinsContainer("http://playground.com/");
+
+    model.load(IntegrationHelper.getResourceFile("F1", "WOAVoorbeeld.ccr").getAbsolutePath());
+
+    model.disableWOA();
+
+    Part landhoofd = new Part(model, "http://www.buildingbits.nl/validatieContainer.rdf#_BB526node1a1hg7ekvx25");
+    log.debug(landhoofd.getDescription());
+
+    Part fundering = new Part(model, "http://www.buildingbits.nl/validatieContainer.rdf#_BB846node1a1hg7ekvx13");
+    log.debug(fundering.getDescription());
+
+    Assembly viaduct = new Assembly(model, "http://www.buildingbits.nl/validatieContainer.rdf#_BB649node1a1hg7ekvx5");
+    log.debug(viaduct.getDescription());
+
+    log.info("instance model");
+    DatasetAsserts.logTriples(model.getJenaModel("http://www.buildingbits.nl/MBIContainer.rdf#"));
+
+    log.info("woa model");
+    DatasetAsserts.logTriples(model.getJenaModel("http://www.coinsweb.nl/voorbeeld#"));
+
+    model.close();
+  }
 
 }
