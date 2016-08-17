3a4c70630bfaf0d0a8a17cb8e50b03252fa027f9
Bastiaan Bijl <bastiaan@sysunite.com>
2016-08-17 16:29:44 +0200

    Add jena validation report to report html

--- coins-api/src/main/java/nl/coinsweb/sdk/validator/Validator.java
+++ coins-api/src/main/java/nl/coinsweb/sdk/validator/Validator.java
@@ -25,6 +25,11 @@
 package nl.coinsweb.sdk.validator;
 
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.ValidityReport;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -34,10 +39,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author Bastiaan Bijl, Sysunite 2016
@@ -73,6 +75,17 @@ public class Validator {
 
     Map<String, Object> data = new HashMap<>();
 
+    // Perform the inferences and check jena validity
+    ValidityReport jenaReport = jenaReasoning();
+    ArrayList<ValidityReport.Report> jenaReports = new ArrayList<>();
+    for (Iterator i = jenaReport.getReports(); i.hasNext(); ) {
+      ValidityReport.Report report = (ValidityReport.Report)i.next();
+      jenaReports.add(report);
+    }
+
+
+    allPassed &= jenaReport.isValid();
+
     // Execute all queries
     Collection<ValidationQuery> allQueries = getValidationQueries().values();
     for(ValidationQuery query : allQueries) {
@@ -83,10 +96,66 @@ public class Validator {
     data.put("filename", model.getCoinsContainer().getContainerId());
     data.put("date", new Date().toString());
     data.put("validation", allPassed);
+    data.put("jenaReport", jenaReport);
+    data.put("jenaReports", jenaReports);
     data.put("queries", allQueries);
 
     writeReport(reportLocation, data);
     return allPassed;
+  }
+
+  private ValidityReport jenaReasoning() {
+
+    OntModel schemaModel = (OntModel) model.getJenaOntModel("http://custom.library.com#");      // todo: find a way to get the relevant schema
+
+    String rules =
+
+        // Rdfs rules (copied from rdfs.rules in jena-core:etc)
+        "[rdf1and4: (?x ?p ?y) -> (?p rdf:type rdf:Property), (?x rdf:type rdfs:Resource), (?y rdf:type rdfs:Resource)]\n" +
+            "[rdfs7b:   (?a rdf:type rdfs:Class) -> (?a rdfs:subClassOf rdfs:Resource)] \n" +
+            "[rdfs2:    (?x ?p ?y), (?p rdfs:domain ?c) -> (?x rdf:type ?c)] \n" +
+            "[rdfs3:    (?x ?p ?y), (?p rdfs:range ?c) -> (?y rdf:type ?c)] \n" +
+            "[rdfs5a:   (?a rdfs:subPropertyOf ?b), (?b rdfs:subPropertyOf ?c) -> (?a rdfs:subPropertyOf ?c)] \n" +
+            "[rdfs5b:   (?a rdf:type rdf:Property) -> (?a rdfs:subPropertyOf ?a)] \n" +
+            "[rdfs6:    (?a ?p ?b), (?p rdfs:subPropertyOf ?q) -> (?a ?q ?b)] \n" +
+            "[rdfs7:    (?a rdf:type rdfs:Class) -> (?a rdfs:subClassOf ?a)]\n" +
+            "[rdfs8:    (?a rdfs:subClassOf ?b), (?b rdfs:subClassOf ?c) -> (?a rdfs:subClassOf ?c)] \n" +
+            "[rdfs9:    (?x rdfs:subClassOf ?y), (?a rdf:type ?x) -> (?a rdf:type ?y)] \n" +
+            "[rdfs10:   (?x rdf:type rdfs:ContainerMembershipProperty) -> (?x rdfs:subPropertyOf rdfs:member)] \n" +
+            "\n" +
+
+            // Inference rules (based on queries in COINS 2.0_Semantiek_ConceptVersieV5.docx)
+            "[caxsco:   (?x a ?c1), (?c1 rdfs:subClassOf ?c2) -> (?x a ?c2)]\n" +
+            "[cls-hv1:  (?x owl:hasValue ?y), (?x owl:onProperty ?p), (?u a ?x) -> (?u ?p ?y)]\n" +
+            "[cls-uni:  (?y a ?ci), (?c owl:unionOf ?x), listContains(?x, ?ci)  -> (?y a ?c)]\n" +              // (?x rdf:rest*/rdf:first ?ci)
+            "[prp-inv:  (?p1 owl:inverseOf ?p2),(?x ?p1 ?y) -> (?y ?p2 ?x)]\n" +
+            "[prp-inv2: (?p1 owl:inverseOf ?p2), (?x ?p2 ?y) -> (?y ?p1 ?x)]\n" +
+            "[prp-spo1: (?p1 rdfs:subPropertyOf ?p2), (?x ?p1 ?y) -> (?x ?p2 ?y)]\n" +
+            "[prp-trp:  (?p a owl:TransitiveProperty), (?x ?p ?y), (?y ?p ?z) -> (?x ?p ?z)]\n" +
+            "[scm-avf1: (?c1 owl:allValuesFrom ?y1), (?y1 rdfs:subClassOf ?y2), (?c2 owl:allValuesFrom ?y2), (?c1 owl:onProperty ?p), (?c2 owl:onProperty ?p) -> (?c1 rdfs:subClassOf ?c2)]\n" +
+            "[scm-avf2: (?c1 owl:allValuesFrom ?y), (?c1 owl:onProperty ?p1), (?c2 owl:allValuesFrom ?y), (?c2 owl:onProperty ?p2), (?p1 rdfs:subPropertyOf ?p2) -> (?c2 rdfs:subClassOf ?c1)]\n" +
+            "[scm-dom2: (?p2 rdfs:domain ?c), (?p1 rdfs:subPropertyOf ?p2) -> (?p1 rdfs:domain ?c)]\n" +
+            "[scm-hv:   (?c1 owl:hasValue ?i), (?c1 owl:onProperty ?p1), (?c2 owl:hasValue ?i), (?c2 owl:onProperty ?p2), (?p1 rdfs:subPropertyOf ?p2) -> (?c1 rdfs:subClassOf ?c2)]\n" +
+            "[scm-int:  (?c owl:intersectionOf ?l), listContains(?l, ?cl)  -> (?c rdfs:subClassOf ?cl )]\n" +                            // /rdf:rest*/rdf:first ?cl
+            "[scm-rng1: (?p rdfs:range ?c1), (?c1 rdfs:subClassOf ?c2) -> (?p rdfs:range ?c2)]\n" +
+            "[scm-rng2: (?p2 rdfs:range ?c), (?p1 rdfs:subPropertyOf ?p2) -> (?p1 rdfs:range ?c)]\n" +
+            "[scm-sco:  (?c1 rdfs:subClassOf ?c2), (?c2 rdfs:subClassOf ?c3) -> (?c1 rdfs:subClassOf ?c3)]\n" +
+            "[scm-spo:  (?p1 rdfs:subPropertyOf ?p2), (?p2 rdfs:subPropertyOf ?p3) -> (?p1 rdfs:subPropertyOf ?p3)]\n" +
+            "[scm-svf1: (?c1 owl:someValuesFrom ?y1), (?c1 owl:onProperty ?p), (?c2 owl:someValuesFrom ?y2), (?c2 owl:onProperty ?p), (?y1 rdfs:subClassOf ?y2) -> (?c1 rdfs:subClassOf ?c2)]\n" +
+            "[scm-svf2: (?c1 owl:someValuesFrom ?y), (?c1 owl:onProperty ?p1), (?c2 owl:someValuesFrom ?y), (?c2 owl:onProperty ?p2), (?p1 rdfs:subPropertyOf ?p2) -> (?c1 rdfs:subClassOf ?c2)]\n" +
+            "\n" +
+
+            // Validation rules (improvised)
+            "[validationFP: (?v rb:validation on()) -> [fpb: (?X rb:violation warning('nonsensical warning', 'If any triple exists, be warned (for no reason)')) <- (?X ?P ?V) ] ]\n" +
+            "[validationFP: (?v rb:validation on()) -> [fpb: (?X rb:violation error('nonsensical error', 'If any triple exists, be warned (for no reason)')) <- (?X ?P ?V) ] ]";
+
+
+    Reasoner reasoner = new GenericRuleReasoner(Rule.parseRules(rules));
+    reasoner.bindSchema(schemaModel);
+//    reasoner.setDerivationLogging(true);
+
+    OntModel instanceModel = (OntModel) model.getJenaOntModel(model.getInstanceNamespace(), reasoner);
+    return instanceModel.validate();
   }
 
 
