390c3eef7e297c13a947c6b1cf90b6560ce88f9f
Bastiaan Bijl <bastiaan@sysunite.com>
2016-08-17 16:27:49 +0200

    Make TDBModelFactory extend JenaModelFactory (remove duplication)

--- coins-api/src/main/java/nl/coinsweb/sdk/jena/TDBModelFactory.java
+++ coins-api/src/main/java/nl/coinsweb/sdk/jena/TDBModelFactory.java
@@ -1,14 +1,8 @@
 package nl.coinsweb.sdk.jena;
 
-import com.hp.hpl.jena.ontology.OntDocumentManager;
-import com.hp.hpl.jena.ontology.OntModel;
-import com.hp.hpl.jena.ontology.OntModelSpec;
-import com.hp.hpl.jena.query.Dataset;
-import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.tdb.TDB;
 import com.hp.hpl.jena.tdb.TDBFactory;
 import nl.coinsweb.sdk.ModelFactory;
-import nl.coinsweb.sdk.Namespace;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.codehaus.plexus.util.FileUtils;
 import org.slf4j.Logger;
@@ -17,12 +11,11 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
 
 /**
  * @author Bastiaan Bijl
  */
-public class TDBModelFactory implements ModelFactory {
+public class TDBModelFactory extends JenaModelFactory implements ModelFactory {
 
   private static final Logger log = LoggerFactory.getLogger(TDBModelFactory.class);
 
@@ -31,13 +24,11 @@ public class TDBModelFactory implements ModelFactory {
 
   private Path path;
 
-  Dataset dataset;
 
 
 
-  //  private OntModelSpec reasoner = OntModelSpec.OWL_MEM_MICRO_RULE_INF;
-  private OntModelSpec reasoner = OntModelSpec.OWL_MEM_RDFS_INF;
-  //  private OntModelSpec reasoner = OntModelSpec.OWL_MEM;
+
+
 
   public TDBModelFactory() {
     log.info("start new tdb for namespace given file");
@@ -48,49 +39,6 @@ public class TDBModelFactory implements ModelFactory {
     dataset = TDBFactory.createDataset(path.toString());
   }
 
-  @Override
-  public void setReasoner(OntModelSpec modelSpec) {
-    reasoner = modelSpec;
-  }
-
-  @Override
-  public Model getEmptyModel() {
-    return com.hp.hpl.jena.rdf.model.ModelFactory.createDefaultModel();
-  }
-
-  @Override
-  public OntModel asOntModel(Model model) {
-
-    // Set document manager policy file
-    OntDocumentManager dm = new OntDocumentManager();
-    OntModelSpec modelSpec = reasoner;
-    modelSpec.setDocumentManager(dm);
-    dm.setProcessImports(false);
-    OntModel result = com.hp.hpl.jena.rdf.model.ModelFactory.createOntologyModel(modelSpec, model);
-
-    return result;
-  }
-
-  @Override
-  public Dataset getDataset(Namespace instanceNamespace, Model instanceModel,
-                            Namespace woaNamespace, Model woaModel,
-                            Map<Namespace, Model> libraryModels) {
-
-    refreshModel(instanceNamespace, instanceModel);
-    refreshModel(woaNamespace, woaModel);
-    for(Namespace ns : libraryModels.keySet()) {
-      refreshModel(ns, libraryModels.get(ns));
-    }
-    return dataset;
-  }
-
-  private void refreshModel(Namespace ns, Model model) {
-    if(dataset.containsNamedModel(ns.toString())) {
-      dataset.replaceNamedModel(ns.toString(), model);
-    } else {
-      dataset.addNamedModel(ns.toString(), model);
-    }
-  }
 
 
 
