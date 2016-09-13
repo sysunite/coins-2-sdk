523cd8660818404c3f02ad50f5a0ab77917fa6ec
Bastiaan Bijl <bastiaan@sysunite.com>
2016-09-13 17:04:00 +0200

    Attachments no longer a set but a list

--- coins-api/src/main/java/nl/coinsweb/sdk/CoinsContainer.java
+++ coins-api/src/main/java/nl/coinsweb/sdk/CoinsContainer.java
@@ -28,7 +28,6 @@ import com.hp.hpl.jena.rdf.model.Model;
 
 import java.io.File;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author Bastiaan Bijl, Sysunite 2016
@@ -104,7 +103,7 @@ public interface CoinsContainer {
    *
    * @return  list of file names ('map.pdf', 'pic.jpg')
    */
-  public Set<String> getAttachments();
+  public List<String> getAttachments();
 
   /**
    * Return a pointer to an attachment file.
