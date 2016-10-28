d17438f316ef3847c282d0bd81ff33065f61b372
Bastiaan Bijl <bastiaan@sysunite.com>
2016-10-28 11:02:01 +0200

    Update resolving paths mechanism

--- coins-cli/src/main/java/nl/coinsweb/sdk/cli/CliOptions.java
+++ coins-cli/src/main/java/nl/coinsweb/sdk/cli/CliOptions.java
@@ -109,7 +109,9 @@ public class CliOptions {
   public static Path resolvePath(String path) {
 
     try {
-      Path currentPath = Paths.get(new File(System.getProperty("user.dir")).getCanonicalPath());
+      String userDir = System.getProperty("user.dir");
+      log.info("Resolving path "+path.toString()+" to user dir: "+userDir);
+      Path currentPath = Paths.get(new File(userDir).getCanonicalPath());
       return currentPath.resolve(path);
     } catch (IOException e) {
       log.warn("Failed to locate path "+path+" relative to user dir "+System.getProperty("user.dir"));
